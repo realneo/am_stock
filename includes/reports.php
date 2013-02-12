d7742c37212d54fd46a7465dd09cad7b2aa3130f
Nadhir Mubarak Bahayan <nadhir2@hotmail.com>
2013-02-12 16:09:35 +0300

    Make the Report Work

--- includes/reports.php
+++ includes/reports.php
@@ -1,37 +1,38 @@
 <?php  include("db_conn.php");?>
 <table>
     <tr>
-        <th> No </th>
-        <th width="110px"> Date </th>
-        <th> Product </th>
-        <th width="50px"> Quantity </th>
-        <th> Unit </th>
-        <th width="10px"> Delete </th>
+        <th></th>
+        <th>From:
+        <select id="from_date">
+            <?php
+                $q = mysql_query("SELECT DISTINCT `date` FROM `products` WHERE `company_id` = '1' ORDER BY `date` ASC");
+                while($row = mysql_fetch_array($q)){
+                    $from_date = $row['date'];
+                    echo "<option value='{$from_date}'>{$from_date}</option>";
+                }
+            ?>
+        </select>
+        </th>
+        <th>To:
+        <select id="to_date">
+            <?php
+                $qq = mysql_query("SELECT DISTINCT `date` FROM `products` WHERE `company_id` = '1' ORDER BY `date` DESC");
+                while($row = mysql_fetch_array($qq)){
+                    $to_date = $row['date'];
+                    echo "<option value='{$to_date}'>{$to_date}</option>";
+                }
+            ?>
+        </select>        
+        </th>
     </tr>
+</table>
+<table>
+    </tr>
+        <th>No</th>
+        <th>Product</th>
+        <th>Stock</th>
+    </tr>   
     <?php
-        $c_id = $_GET['c_id'];
-        $count = 0;
-        $q = mysql_query("SELECT * FROM `products` WHERE `company_id` = '$c_id'");
-        while($row = mysql_fetch_array($q)){
-           $name = $row['name'];
-           $date = $row['name'];
-           $quantity = $row['quantity'];
-           $unit = $row['unit'];
-           //$category_id = $row['category_id'];
-           $count++;
-           $prod_id = $row['id'];
-           
-           $date = date("d M Y");
-           echo"
-            <tr>
-                <td>{$count}</td>
-                <td>{$date}</td>
-                <td>{$name}</td>
-                <td align='right'>{$quantity}</td>
-                <td>{$unit}</td>
-                <td><a class='delete_prod_btn' id='{$prod_id}'><img src='images/icons/delete.png' alt='Delete' /></a></td>
-            </tr>";
-          
-        }
+        echo $from_date.$to_date;
     ?>
- </table>
+</table>
